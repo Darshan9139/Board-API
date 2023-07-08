00f067b415eb65fca51e8a045a9e384339ba5d95
Darshan <patildarshan9139@gmail.com>
2023-07-08 19:29:12 +0530

    Resolved Undefined property error

--- src/app/mycomponents/search-activity/search-activity.component.ts
+++ src/app/mycomponents/search-activity/search-activity.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { ApiCallService } from '../../myservices/api-call.service'
 
 @Component({
@@ -9,26 +9,21 @@ import { ApiCallService } from '../../myservices/api-call.service'
 })
 
 export class SearchActivityComponent {
-    SelectedCategory:string="Category"
-    participants:number=1
-    Categories= ["education", "recreational", "social", "DIY", "charity", "cooking", "relaxation", "music", "busywork"]
-    activity:any
-  constructor(private activityCall: ApiCallService){}
+  SelectedCategory: string = "recreational"
+  participants: number = 1
+  Categories = ["education", "recreational", "social", "DIY", "charity", "cooking", "relaxation", "music", "busywork"]
+  Activity: any;
+  constructor(private activityCall: ApiCallService) { }
 
   ngOnInit() {
     this.GetRandomActivity()
   }
+
+  async SearchActivity() {
+    this.activityCall.getActivity(this.SelectedCategory, this.participants).subscribe(data=>(this.Activity=data))
   
-  SearchActivity()
-  {
-    this.activityCall.getActivity(this.SelectedCategory,this.participants).subscribe(data=>(
-      this.activity=data
-    ))
   }
-  GetRandomActivity()
-    {
-      this.activityCall.getActivityWithoutPara().subscribe(data=>(
-        this.activity=data
-      ))
-    }
+  GetRandomActivity() {
+    this.activityCall.getActivityWithoutPara().subscribe(data=>(this.Activity=data))
+  }
 }
